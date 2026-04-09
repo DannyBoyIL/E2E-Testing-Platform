975045e3e0f79c17b09172dd9bc35477cdfa0f7b
daniellotem <d.r.lotem@gmail.com>
2026-04-09 14:51:12 -0500

    Implements Playwright test POM

--- e2e/pages/LoginPage.ts
+++ e2e/pages/LoginPage.ts
@@ -1,17 +1,21 @@
 import { Page, Locator } from '@playwright/test';
 
 export class LoginPage {
+
     readonly emailInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
+
     constructor(private readonly page: Page) {
         this.emailInput = page.getByPlaceholder('Email');
         this.passwordInput = page.getByPlaceholder('Password');
         this.loginButton = page.getByRole('button', { name: 'Login' });
     }
+
     async goto() {
         await this.page.goto('/login');
     }
+
     async login(email: string, password: string) {
         await this.emailInput.fill(email);
         await this.passwordInput.fill(password);
--- e2e/pages/RegisterPage.ts
+++ e2e/pages/RegisterPage.ts
@@ -1,11 +1,13 @@
 import { Page, Locator } from '@playwright/test';
 
 export class RegisterPage {
+
     readonly nameInput: Locator;
     readonly emailInput: Locator;
     readonly passwordInput: Locator;
     readonly confirmPasswordInput: Locator;
     readonly registerButton: Locator;
+
     constructor(private readonly page: Page) {
         this.nameInput = page.getByPlaceholder('Name');
         this.emailInput = page.getByPlaceholder('Email');
@@ -13,9 +15,11 @@ export class RegisterPage {
         this.confirmPasswordInput = page.getByPlaceholder('Confirm Password');
         this.registerButton = page.getByRole('button', { name: 'Register' });
     }
+
     async goto() {
         await this.page.goto('/register');
     }
+
     async register(name: string, email: string, password: string) {
         await this.nameInput.fill(name);
         await this.emailInput.fill(email);
